b720101ccd2ec6df15b7f24309976690c4af0eae
jskick <jskick@gmail.com>
2020-01-06 20:07:28 -0500

    final code

---
+++ StarterCode/assets/js/app.js
@@ -0,0 +1,105 @@
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+var svg = d3.select(".chart")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import Data
+d3.csv("assets/data/data.csv").then(function(stateData) {
+
+
+    stateData.forEach(function(data) {
+      data.healthcare = +data.healthcare;
+      data.poverty = +data.poverty;
+    });
+
+    var xLinearScale = d3.scaleLinear()
+      .domain([d3.min(stateData, d => d.poverty*0.8), d3.max(stateData, d => d.poverty*1.1)])
+      .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+      .domain([0, d3.max(stateData, d => d.healthcare)])
+      .range([height, 0]);
+
+
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+
+    chartGroup.append("g")
+      .call(leftAxis);
+
+
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(stateData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", "15")
+    .attr("fill", "blue")
+    .classed("stateCircle", true)
+    .attr("opacity", ".9");
+
+
+  var textGroup = chartGroup.selectAll(".stateText")
+    .data(stateData)
+    .enter()
+    .append("text")
+    .attr("x", d => xLinearScale(d.poverty))
+    .attr("y", d => yLinearScale(d.healthcare))
+    .classed("stateText", true)
+    .text(function(d){return d.abbr})
+
+    var toolTip = d3.tip()
+      .attr("class", "tooltip")
+      .offset([80, -60])
+      .html(function(d) {
+        return (`${d.state}<br>Povery: ${d.poverty}<br>Healthcare: ${d.healthcare}`);
+      });
+
+
+    chartGroup.call(toolTip);
+
+
+    circlesGroup.on("click", function(data) {
+      toolTip.show(data, this);
+    })
+      .on("mouseout", function(data, index) {
+        toolTip.hide(data);
+      });
+
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 40)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Lacks Healthcare(%)");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("class", "axisText")
+      .text("In poverty (%)");
+  }).catch(function(error) {
+    console.log(error);
+  });
